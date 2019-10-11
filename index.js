4c839b244c7ac2f22fc473e36185b2f261d92a34
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-10-11 19:55:40 -0400

    configured node.js and added socket.io

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
+const app = express()
+const http = require('http').Server(app)
 const path = require('path')
+const io = require('socket.io')(http);
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/game.html')))
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+  
+http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
