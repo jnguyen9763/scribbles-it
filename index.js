93b92b58876b3d5bf3180cc672be3836b81f6753
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-10-12 00:15:33 -0400

    added random word

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const path = require('path')
-const io = require('socket.io')(http);
+const io = require('socket.io')(http)
 const PORT = process.env.PORT || 5000
 
 app.use(express.static(path.join(__dirname, 'public')));
--- public/script.js
+++ public/script.js
@@ -2,10 +2,12 @@ var socket = io();
 
 var brushColor = '#000';
 var strokeWidth = 15;
+var canvasWidth = document.getElementById('canvas').clientWidth;
+var canvasHeight = document.getElementById('canvas').clientHeight;
+
+var words = ["time", "year", "people", "way", "day", "man", "thing", "woman", "life", "child", "world", "school", "state", "family", "student", "group", "country", "problem", "hand", "part", "place", "case", "week", "company", "system", "program", "question", "work", "government", "number", "night", "point", "home", "water", "room", "mother", "area", "money", "story", "fact", "month", "lot", "right", "study", "book", "eye", "job", "word", "business", "issue", "side", "kind", "head", "house", "service", "friend", "father", "power", "hour", "game", "line", "end", "member", "law", "car", "city", "community", "name,", "president", "team", "minute", "idea", "kid", "body", "information", "back", "parent", "face", "others", "level", "office", "door", "health", "person", "art", "war", "history", "party", "result", "change", "morning", "reason", "research", "girl", "guy", "moment", "air", "teacher", "force", "education"];
 
 function setup() {
-  var canvasWidth = document.getElementById('canvas').clientWidth;
-  var canvasHeight = document.getElementById('canvas').clientHeight;
   var canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('canvas');
   background('#FFF');
@@ -20,18 +22,21 @@ function newDrawing(data) {
 }
 
 function mouseDragged() {
+  // console.log(words[Math.floor(Math.random() * Math.floor(words.length))]);
   stroke(brushColor);
   strokeWeight(strokeWidth);
   line(mouseX, mouseY, pmouseX, pmouseY);
   sendMouse(mouseX, mouseY, pmouseX, pmouseY);
 }
 
-function sendMouse(x, y, pX, pY) {
+function sendMouse(x, y, pX, pY, width, height) {
   const data = {
     x: x,
     y: y,
     px: pX,
     py: pY,
+    width: width,
+    height: height,
     color: brushColor,
     strokeWidth: strokeWidth
   }
