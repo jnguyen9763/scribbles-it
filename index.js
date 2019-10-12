8f133184b0dbe77b9db0904ba4830f49531ca217
Jacqueline Nguyen <jnguyen9763@gmail.com>
2019-10-12 01:14:23 -0400

    Added multiplayer functionality

--- index.js
+++ index.js
@@ -9,13 +9,65 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/game.html')))
 
-io.on('connection', function(socket){
+var players = [];
+var gameRunning = false;
+var word = "";
+
+io.on('connection', function(socket) {
   console.log('a user connected');
+  players.push(socket.id);
+
+  if (players.length > 1 && !gameRunning) {
+    gameRunning = true
+    var id = players[Math.floor(Math.random() * Math.floor(players.length))]
+    if (socket.id === id) {
+      socket.emit('drawer');
+    }
+    else {
+      socket.broadcast.to(id).emit('drawer')
+    }
+  }
+
   socket.on('mouse', (data) => socket.broadcast.emit('mouse', data))
-  socket.on('disconnect', () => console.log('Client has disconnected'))
+  socket.on('word', (w) => word = w)
+
+  socket.on('reset game', function() {
+    io.emit('reset drawer');
+    if (players.length > 1) {
+      var id = players[Math.floor(Math.random() * Math.floor(players.length))]
+      if (socket.id === id) {
+        socket.emit('drawer');
+      }
+      else {
+        socket.broadcast.to(id).emit('drawer')
+      }
+    }
+    else {
+      gameRunning = false
+    }
+  })
+
   socket.on('chat message', function(msg) {
-    io.emit('chat message', msg);
-  });
-});
+    if (msg === word) {
+      socket.emit('winner')
+    }
+    io.emit('chat message', msg)
+  })
+
+  socket.on('disconnect', function() {
+    console.log('Client has disconnected');
+    for (var i = 0; i < players.length; i++) {
+      if (players[i] == socket.id) {
+        players.splice(i, 1);
+        break;
+      }
+    }
+
+    if (players.length <= 1) {
+      gameRunning = false;
+      io.emit('reset drawer');
+    }
+  }) 
+})
 
 http.listen(PORT, () => console.log(`Listening on ${ PORT }`))
--- public/script.js
+++ public/script.js
@@ -4,7 +4,7 @@ function sendMessage(event) {
   event.preventDefault();
   var msg = document.querySelector('#text').value;
   document.querySelector('#text').value = '';
-  if (/\S/.test(msg))
+  if (/\S/.test(msg) && !isDrawer)
     socket.emit('chat message', msg);
 }
 
@@ -15,6 +15,7 @@ var brushColor = '#000';
 var strokeWidth = 15;
 var canvasWidth = document.getElementById('canvas').clientWidth;
 var canvasHeight = document.getElementById('canvas').clientHeight;
+var isDrawer = false;
 
 var words = ["time", "year", "people", "way", "day", "man", "thing", "woman", "life", "child", "world", "school", "state", "family", "student", "group", "country", "problem", "hand", "part", "place", "case", "week", "company", "system", "program", "question", "work", "government", "number", "night", "point", "home", "water", "room", "mother", "area", "money", "story", "fact", "month", "lot", "right", "study", "book", "eye", "job", "word", "business", "issue", "side", "kind", "head", "house", "service", "friend", "father", "power", "hour", "game", "line", "end", "member", "law", "car", "city", "community", "name,", "president", "team", "minute", "idea", "kid", "body", "information", "back", "parent", "face", "others", "level", "office", "door", "health", "person", "art", "war", "history", "party", "result", "change", "morning", "reason", "research", "girl", "guy", "moment", "air", "teacher", "force", "education"];
 
@@ -22,12 +23,27 @@ function setup() {
   var canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('canvas');
   background('#FFF');
-
   socket.on('mouse', newDrawing);
 }
 
-socket.on('chat message', function(msg){
+socket.on('chat message', function(msg) {
   $('#messages').prepend($('<li>').text(msg));
+});
+
+socket.on('drawer', function() {
+  isDrawer = true;
+  var word = words[Math.floor(Math.random() * Math.floor(words.length))];
+  alert("You are the drawer! Your word is " + word + ".");
+  socket.emit('word', word);
+});
+
+socket.on('reset drawer', function() {
+  isDrawer = false;
+});
+
+socket.on('winner', function() {
+  alert('You are the winner!');
+  socket.emit('reset game');
 });
 
 function newDrawing(data) {
@@ -37,11 +53,12 @@ function newDrawing(data) {
 }
 
 function mouseDragged() {
-  // console.log(words[Math.floor(Math.random() * Math.floor(words.length))]);
-  stroke(brushColor);
-  strokeWeight(strokeWidth);
-  line(mouseX, mouseY, pmouseX, pmouseY);
-  sendMouse(mouseX, mouseY, pmouseX, pmouseY);
+  if (isDrawer) {
+    stroke(brushColor);
+    strokeWeight(strokeWidth);
+    line(mouseX, mouseY, pmouseX, pmouseY);
+    sendMouse(mouseX, mouseY, pmouseX, pmouseY);
+  } 
 }
 
 function sendMouse(x, y, pX, pY, width, height) {
