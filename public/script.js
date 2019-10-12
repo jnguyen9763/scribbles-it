1fdb5502d3890bea26fd87c81d03f3a94d2e764b
Linh Tran <linh.tran@tufts.edu>
2019-10-11 21:24:40 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,1 +1,17 @@
 var socket = io();
+
+function setup() {
+  createCanvas(1000, 580);
+  background(200);
+  line(0, 0, width, height);
+}
+
+function draw() {
+  if (mouseIsPressed) {
+     fill(0);
+  } else {
+     fill(255);
+  }
+
+  ellipse(mouseX, mouseY, 20, 20);
+}
